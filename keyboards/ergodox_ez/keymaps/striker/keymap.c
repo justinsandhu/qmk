fc18c50a1f21a6ae639eb28f43a37df382ea6f58
justin <justin@ghost.lan>
2020-09-27 12:49:51 -0500

    nothing to see here

--- keyboards/ergodox_ez/keymaps/striker/keymap.c
+++ keyboards/ergodox_ez/keymaps/striker/keymap.c
@@ -63,10 +63,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_DELETE,      KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_BSPACE,                                      KC_PIPE,        KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLASH,
     KC_LCTRL,       KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_QUOTE,
     KC_LSHIFT,      KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           TO(1),/*Disabled for now*/                      KC_TAB,         KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_RSHIFT,
-    KC_GRAVE,       KC_NO,     KC_NO,     KC_NO,       KC_NO, 	                                                                                                    KC_LALT,        KC_DOWN,        KC_LBRACKET,    KC_RBRACKET,    KC_RCTL,
-                                                                                                                 KC_NO,KC_NO,          KC_NO,KC_ESCAPE,
-                                                                                      /*OS mod*/      /*Mod*/          MO(2),          MO(4),          /*knees weak, arms are heavy*/
-                                                                                      TMUX_MOD,           KC_LALT,        MO(5),          MT(MO(6), TMUX_MOD),          KC_SPACE,       KC_ENTER
+    KC_GRAVE,       KC_NO,          KC_NO,          KC_NO,          KC_NO, 	                                                                                                    KC_LALT,        KC_DOWN,        KC_LBRACKET,    KC_RBRACKET,    KC_RCTL,
+                                                                                                                   KC_NO,KC_NO,          KC_NO,KC_ESCAPE,
+                                                                                      /*Tmux mod*/      /*Mod*/          MO(2),          MO(4),          /*knees weak, arms are heavy*/
+                                                                                      TMUX_MOD,       KC_LALT,           MO(5),          MO(6),         KC_SPACE,       KC_ENTER
   ), // VIM Mode
   [1] = LAYOUT_ergodox_pretty(
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, TO(0),                                          KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
     KC_GRAVE,       KITTY_UNI,      KITTY_HINT,     KITTY_SB,       KITTY_MOD, 	                                                                                                    KC_LALT,        KC_DOWN,        KC_LBRACKET,    KC_RBRACKET,    KC_RCTL,
                                                                                                                  KC_NO,KC_NO,       KC_NO,KC_ESCAPE,
                                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT,
